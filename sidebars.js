69087b6fd5508c8c238a83c70ec75cfc131a9419
alt <84208222+a1ttech@users.noreply.github.com>
2024-10-28 22:46:59 -0700

    v7 updates

--- sidebars.js
+++ sidebars.js
@@ -88,6 +88,7 @@ const sidebars = {
             keywords: ['migrations'],
           },
           items: [
+            'sdk/migrations/7.0.0',
             'sdk/migrations/6.0.0',
             'sdk/migrations/4.0.0',
             'sdk/migrations/3.2.0',
@@ -556,6 +557,7 @@ const sidebars = {
             keywords: ['api-reference'],
           },
           items: [
+            'api-reference/v6-sdk',
             'api-reference/v3-sdk',
             'api-reference/v2-sdk',
             'api-reference/v1-sdk',
